884b6aaac1f1f7f733eda259db2539f684875b66
deox123 <nikolamilosevic93@yahoo.com>
2022-04-17 18:38:03 +0200

    add server and place-info controller

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,29 @@
-export default function add(x: number, y: number, z: number): number {
-  return x + y + z;
-}
+import express from 'express';
+
+import { transformPlaceInfo, fetchPlaceInfo } from './utils';
+
+const server = express();
+
+server.get('/place-info', async function (req, res) {
+  const { placeId } = req.query;
+
+  if (typeof placeId === 'string') {
+    try {
+      const placeInfo = await fetchPlaceInfo(placeId);
+      const transformedPlaceInfo = transformPlaceInfo(placeInfo);
+      res.json(transformedPlaceInfo);
+    } catch {
+      res.status(500).send('Places service is down, try later');
+    }
+  } else {
+    res.send(
+      'Please provide place id in string format, such as placeId={placeId}'
+    );
+  }
+});
+
+const port = 4000;
+
+server.listen(port, () => {
+  console.log(`Server listening at ${port}`);
+});
